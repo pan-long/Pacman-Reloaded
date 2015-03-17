4096fbdf848522f2630d7840973dca051ea09f34
Su Xuan <czsx1415@gmail.com>
2015-03-17 21:08:37 +0800

    Added a dummy pacman

--- Pacman Reloaded/Pacman Reloaded/GameScene.swift
+++ Pacman Reloaded/Pacman Reloaded/GameScene.swift
@@ -9,34 +9,24 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    // Assume there is only one pacman for now.
+    // TODO
+    let pacman = SKSpriteNode(imageNamed: "pacman")
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
-        self.addChild(myLabel)
+        backgroundColor = SKColor.whiteColor()
+        
+        pacman.position = CGPoint(x: size.width/2 - pacman.size.width/2,
+            y: size.height/2 - pacman.size.height/2)
+        
+        addChild(pacman)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
     }
    
     override func update(currentTime: CFTimeInterval) {
