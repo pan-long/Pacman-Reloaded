951dd31961b1914953612a9080af14373cceea3f
Minqi Chen <cnchenminqi@gmail.com>
2015-04-11 17:07:47 +0800

    Turn around when frightened

--- Pacman Reloaded/Pacman Reloaded/Ghost.swift
+++ Pacman Reloaded/Pacman Reloaded/Ghost.swift
@@ -14,6 +14,7 @@ class Ghost: MovableObject {
 
     var frightened: Bool = false {
         didSet {
+            self.currentDir = self.currentDir.opposite
             updateTexture()
         }
     }
