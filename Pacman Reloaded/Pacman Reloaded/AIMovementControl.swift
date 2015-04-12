fa1f304bc9c06df3cd37afbd5b79bf161f6cbe4b
choyukchow <choyukchow@gmail.com>
2015-04-12 17:07:05 +0800

    Modification of scatter mode

--- Pacman Reloaded/Pacman Reloaded/AIMovementControl.swift
+++ Pacman Reloaded/Pacman Reloaded/AIMovementControl.swift
@@ -9,8 +9,19 @@
 import Foundation
 import SpriteKit
 
-enum MovementMode {
+enum GhostMovementMode {
     case Chase, Scatter, Frightened
+    
+    var str: String {
+        switch self {
+        case .Chase:
+            return "Chase"
+        case .Scatter:
+            return "Scatter"
+        default:
+            return "Frightened"
+        }
+    }
 }
 
 class AIMovementControl: MovementControl {
@@ -22,7 +33,7 @@ class AIMovementControl: MovementControl {
     weak var dataSource: MovementDataSource!
     
     private var counter = 0
-    private var currentMode = MovementMode.Scatter
+    private var currentMode = GhostMovementMode.Scatter
     private var currentModeDuration = 0
     private var shouldUpdate = true
     
@@ -30,35 +41,125 @@ class AIMovementControl: MovementControl {
         self.movableObject = movableObject
     }
     
-    private func calculateDistance(firstPostion: CGPoint, secondPostion: CGPoint) -> Double  {
-        let distanceX = Double(firstPostion.x) - Double(secondPostion.x)
-        let distanceY = Double(firstPostion.y) - Double(secondPostion.y)
-        return sqrt(distanceX * distanceX + distanceY * distanceY)
+    // MARK: Update for movement control
+    
+    func update() {
+        if isUpdateFrame() {
+            let ghost = movableObject as Ghost
+            
+            // Ghost moves randomly when it's frightened
+            if ghost.frightened {
+                frightenUpdate()
+                return
+            }
+            
+            // If counter exceed indefinite chase -> chase update
+            if counter > INDEFINITE_CHASE {
+                println("Indefinite chase")
+                chaseUpdate()
+            }
+            counter += 1
+            
+            // Update movable object's direction
+            switch currentMode {
+            case .Scatter:
+                if currentModeDuration > SCATTER_MODE_DURATION {
+                    changeMode(.Chase)
+                } else {
+                    scatterUpdate()
+                }
+            case .Chase:
+                if currentModeDuration > CHASE_MODE_DURATION {
+                    changeMode(.Scatter)
+                } else {
+                    chaseUpdate()
+                }
+            default:
+                break
+            }
+            currentModeDuration += 1
+        }
     }
     
-    func scatterUpdate() {
+    private func isUpdateFrame() -> Bool {
+        shouldUpdate = !shouldUpdate
+        return shouldUpdate
+    }
+    
+    private func reverseDirection() {
+        movableObject.changeDirection(movableObject.currentDir.opposite)
+    }
+    
+    private func changeMode(nextMode: GhostMovementMode) {
+        println("\(nextMode.str)")
+        reverseDirection()
+        currentMode = nextMode
+        currentModeDuration = 0
+    }
+    
+    func reset() {
+        counter = 0
+        currentMode = GhostMovementMode.Scatter
+        currentModeDuration = 0
+        shouldUpdate = true
+    }
+    
+    // MARK: AI Frightened Mode
+    
+    func frightenUpdate() {
         let availableDirections = movableObject.getAvailableDirections()
         if availableDirections.count > 0 {
             let superDice = Int(arc4random_uniform(UInt32(availableDirections.count)))
-
+            
             movableObject.changeDirection(availableDirections[superDice])
         }
     }
     
-    func getChaseTarget(visibleObject: MovableObject) -> CGPoint {
+    // MARK: AI Scatter Mode
+    
+    func scatterUpdate() {
+        let availableDirections = movableObject.getAvailableDirections()
+        var nextDirection: Direction
+        
+        if availableDirections.isEmpty {
+            nextDirection = .None
+        } else {
+            nextDirection = availableDirections[0]
+        }
+        
+        var minDistanceFromHome: Double = 100000
+        
+        for direction in availableDirections {
+            let distance = calculateDistance(
+                movableObject.getNextPosition(direction),
+                secondPostion: getHome())
+            
+            if distance < minDistanceFromHome {
+                minDistanceFromHome = distance
+                nextDirection = direction
+            }
+        }
+        
+        movableObject.changeDirection(nextDirection)
+    }
+
+    
+    func getHome() -> CGPoint {
         return CGPoint(x: 0, y: 0)
     }
+    
+    // MARK: AI Chase Mode
     
     func chaseUpdate() {
         let availableDirections = movableObject.getAvailableDirections()
         var nextDirection: Direction
-
+        
         if availableDirections.isEmpty {
             nextDirection = .None
         } else {
-             nextDirection = availableDirections[0]
+            nextDirection = availableDirections[0]
         }
-
+        
         var minDistanceToPacman: Double = 100000
         
         for direction in availableDirections {
@@ -76,66 +177,15 @@ class AIMovementControl: MovementControl {
         
         movableObject.changeDirection(nextDirection)
     }
-
-    private func isUpdateFrame() -> Bool {
-        shouldUpdate = !shouldUpdate
-        return shouldUpdate
+    
+    func getChaseTarget(visibleObject: MovableObject) -> CGPoint {
+        return CGPoint(x: 0, y: 0)
     }
     
-    private func reverseDirection() {
-        movableObject.changeDirection(movableObject.currentDir.opposite)
-    }
-    
-    func reset() {
-        counter = 0
-        currentMode = MovementMode.Scatter
-        currentModeDuration = 0
-        shouldUpdate = true
-    }
-    
-    func update() {
-        if isUpdateFrame() {
-            let ghost = movableObject as Ghost
-            
-            // Ghost moves randomly when it's frightened
-            if ghost.frightened {
-                scatterUpdate()
-                return
-            }
-            
-            // If counter exceed indefinite chase -> chase update
-            if counter > INDEFINITE_CHASE {
-                println("Indefinite chase")
-                chaseUpdate()
-            }
-            counter += 1
-            
-            // Update movable object's direction
-            switch currentMode {
-            case .Scatter:
-                if currentModeDuration > SCATTER_MODE_DURATION {
-                    println("Chase Mode")
-                    reverseDirection()
-                    currentMode = .Chase
-                    currentModeDuration = 0
-                } else {
-                    scatterUpdate()
-                    currentModeDuration += 1
-                }
-            case .Chase:
-                if currentModeDuration > CHASE_MODE_DURATION {
-                    println("Scatter Mode")
-                    reverseDirection()
-                    currentMode = .Scatter
-                    currentModeDuration = 0
-                } else {
-                    chaseUpdate()
-                    currentModeDuration += 1
-                }
-            default:
-                break
-            }
-        }
+    private func calculateDistance(firstPostion: CGPoint, secondPostion: CGPoint) -> Double  {
+        let distanceX = Double(firstPostion.x) - Double(secondPostion.x)
+        let distanceY = Double(firstPostion.y) - Double(secondPostion.y)
+        return sqrt(distanceX * distanceX + distanceY * distanceY)
     }
 }
 
