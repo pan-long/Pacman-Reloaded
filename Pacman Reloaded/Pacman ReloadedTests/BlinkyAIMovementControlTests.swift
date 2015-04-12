68de0895c8a439aeb97f16aeea0e53184394886f
choyukchow <choyukchow@gmail.com>
2015-04-12 22:32:33 +0800

    Set up Blinky AI Movement Control Tests

--- Pacman Reloaded/Pacman ReloadedTests/BlinkyAIMovementControlTests.swift
+++ Pacman Reloaded/Pacman ReloadedTests/BlinkyAIMovementControlTests.swift
@@ -6,4 +6,37 @@
 //  Copyright (c) 2015 cs3217. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import XCTest
+
+class BlinkyAIMovementControlTests: XCTestCase {
+    private var blinky = Ghost(imageName: "ghost-red")
+    private var pinky = Ghost(imageName: "ghost-yellow")
+    private var inky = Ghost(imageName: "ghost-blue")
+    private var clyde = Ghost(imageName: "ghost-orange")
+    private let pacman = PacMan()
+    
+    private var ghosts: [MovableObject]!
+    private var pacmans: [MovableObject]!
+    
+    private var blinkyMovement: BlinkyAIMovememntControl!
+    
+    private func constructBlinkyAIMovementControl() {
+        ghosts = [blinky, pinky, inky, clyde]
+        pacmans = [pacman]
+        blinkyMovement = BlinkyAIMovememntControl(movableObject: blinky)
+        blinkyMovement.dataSource = self
+    }
+}
+
+extension BlinkyAIMovementControlTests: MovementDataSource {
+    func getPacmans() -> [MovableObject] {
+        var pacmans = [MovableObject]()
+        pacmans.append(pacman)
+        return pacmans
+    }
+    
+    func getBlinky() -> MovableObject {
+        return blinky
+    }
+}
